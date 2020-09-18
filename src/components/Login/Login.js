0c3917de36e325b6ea07e17430d071990b02a75c
mathew94 <mathewrodrick94@hotmail.com>
2020-09-18 11:27:55 +0600

    edit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -25,14 +25,8 @@ const Login = () => {
             history.replace(from);
             // ...
           }).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
+            const errorMessage = error.message;
+            console.log(errorMessage);
           });
     }
     return (
