ab29689c662c15ee563a60ce9055dbe90ddd61c5
mathew94 <mathewrodrick94@hotmail.com>
2020-09-18 11:17:11 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB0JaO4zEWhN8wigJRZ0x0KURVQ5umk-d0",
+    authDomain: "burj-al-arab-94014.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-94014.firebaseio.com",
+    projectId: "burj-al-arab-94014",
+    storageBucket: "burj-al-arab-94014.appspot.com",
+    messagingSenderId: "699036097162",
+    appId: "1:699036097162:web:0bd2e9e053bd3bf858eb73"
+  };
+  export default firebaseConfig;
